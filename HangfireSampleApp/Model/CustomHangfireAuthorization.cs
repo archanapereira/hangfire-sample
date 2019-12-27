55ca8c905236bfaa7636c5580d7d9720614777f3
Archana Pereira <archanapereira@gmail.com>
2019-12-27 18:22:45 +0000

    Add project files.

---
+++ HangfireSampleApp/Model/CustomHangfireAuthorization.cs
@@ -0,0 +1,15 @@
+﻿using Hangfire.Dashboard;
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace HangfireSampleApp.Model
+{
+    public class CustomHangfireAuthorization : IDashboardAuthorizationFilter
+    {
+        public bool Authorize([NotNull] DashboardContext context)
+        {
+            var httpContext = context.GetHttpContext();
+            return httpContext.User.Identity.IsAuthenticated;
+        }
+    }
+}
---
+++ HangfireSampleApp/Startup.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using HnagfireSampleApp.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Hangfire;
+using Hangfire.SqlServer;
+using HangfireSampleApp.Model;
+
+namespace HnagfireSampleApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddRazorPages();
+
+            services.AddHangfire(configuration => configuration
+               .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+               .UseSimpleAssemblyNameTypeSerializer()
+               .UseRecommendedSerializerSettings()
+               .UseSqlServerStorage(Configuration.GetConnectionString("HangfireConnection"), new SqlServerStorageOptions
+               {
+                   CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                   SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                   QueuePollInterval = TimeSpan.Zero,
+                   UseRecommendedIsolationLevel = true,
+                   UsePageLocksOnDequeue = true,
+                   DisableGlobalLocks = true
+               }));
+
+            services.AddHangfireServer();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+
+            //app.UseHangfireDashboard();
+
+            app.UseHangfireDashboard("/hangfireDashboard", new DashboardOptions{ 
+               Authorization = new[] {new CustomHangfireAuthorization()}
+            });
+
+            //This will execute a job at the start of the application.
+            //BackgroundJob.Enqueue(() => Console.WriteLine("Hello world from Hangfire!"));
+
+            //run a recurring job every minute
+            RecurringJob.AddOrUpdate(() => Console.WriteLine($"This is a recurring job from HangfireSampleApp!!!"), Cron.Minutely);
+        }
+    }
+}
